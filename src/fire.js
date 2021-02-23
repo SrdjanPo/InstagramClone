f3c22ef50cb03a789383a8096586323feb70dd35
SrdjanPo <ponjavusic.srdjan@gmail.com>
2021-02-23 15:47:57 +0100

    first commit

---
+++ src/fire.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+var firebaseConfig = {
+  apiKey: "AIzaSyANS93GDRG13vnMcsZNI9YF6HO9VomSW0Q",
+  authDomain: "insta-clone-871b8.firebaseapp.com",
+  projectId: "insta-clone-871b8",
+  storageBucket: "insta-clone-871b8.appspot.com",
+  messagingSenderId: "910418758506",
+  appId: "1:910418758506:web:c0a7b5e8d83d141277f313",
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
